83853d551d78a5559b64f1155d878706ec027a8a
Đại Trần Đình <trandinhdai_t64@hus.edu.vn>
2023-03-07 11:39:29 +0700

    start project

---
+++ nmanage_library/src/main/java/com/nashtech/nmanage_library/NmanageLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.nashtech.nmanage_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmanageLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NmanageLibraryApplication.class, args);
+	}
+
+}
---
+++ nmanage_library/src/test/java/com/nashtech/nmanage_library/NmanageLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.nmanage_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NmanageLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
